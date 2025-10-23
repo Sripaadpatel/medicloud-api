345cb16dbc477387fc6864c5814078e448a57838
sripaadpatel <sripaad50724@gmail.com>
2025-10-23 23:40:01 +0530

    Initial commit: Spring Boot project setup

---
+++ src/main/java/com/medicloud/MedicloudApiApplication.java
@@ -0,0 +1,13 @@
+package com.medicloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicloudApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedicloudApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/medicloud/MedicloudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medicloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicloudApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
